58d4591b1e7d2fe6cca9ac4e0dbef63c73f2025b
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-08-08 17:33:50 -0300

    update adicionando componentes e começando estilos

---
+++ src/Components/Formulario/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Botao from '../Botao';
+import './style.scss';
+
+class Formulario extends React.Component {
+    render(){
+        return (
+            <form className="novaTarefa">
+                <div className="inputContainer">
+                    <label htmlFor="tarefa">Adicione nova tarefa</label>
+                    <input type="text" name="tarefa" id="tarefa" placeholder='O que você vai estudar' required/>
+                </div>
+                <div className="inputContainer">
+                    <label htmlFor="tempo">Tempo</label>
+                    <input type="time" name="tempo" id="tempo"  step="1" min="00:00:00" max="01:30:00"  required/>
+                </div>
+                <Botao />
+            </form>
+        );
+    }
+}
+
+export default Formulario;
