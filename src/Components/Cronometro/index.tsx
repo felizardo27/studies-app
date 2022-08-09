44f0190e4bcd451de4466d3a859d7e1999a99c7f
João Pedro Felizardo <jp.felizardo27@gmail.com>
2022-08-09 08:09:11 -0300

    Corrigindo Cronometro com useEffect

--- src/Components/Cronometro/index.tsx
+++ src/Components/Cronometro/index.tsx
@@ -2,7 +2,7 @@ import Botao from "../Botao";
 import Relogio from "./Relogio";
 import styles from './Cronometro.module.scss';
 import { ITarefas } from "../../Interfaces/tarefas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../Common/Utils/time";
 
 interface Props{
@@ -12,9 +12,12 @@ interface Props{
 export default function Cronometro({ selecionado }: Props) {
     
     const [tempo, setTempo] = useState<number>();
-    if (selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
+
+    useEffect( () => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    } ,[selecionado]);
 
     return (
         <div className={styles.cronometro}>
