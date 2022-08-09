9b606391afb81ea42eafa874c5089d9a340e507f
Joao Pedro Felizardo <jp.felizardo27@gmail.com>
2022-08-08 22:05:33 -0300

    Atualizando tarefa

--- src/Components/Lista/Item/index.tsx
+++ src/Components/Lista/Item/index.tsx
@@ -1,7 +1,9 @@
+import { ITarefas } from '../../../Interfaces/tarefas';
 import styles from '../Lista.module.scss'
 
 
-export default function Item({tarefa, tempo}: {tarefa: string, tempo: string}) {
+export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefas) {
+  console.log('Tarefa: ', {tarefa, tempo, selecionado, completado, id})
   return (
     <li className={styles.item}>
       <h3>{tarefa}</h3>
